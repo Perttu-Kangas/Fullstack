29f876c489a9f5940feb976b01cc12ec4788c657
DeeCaaD <perttu.kangas@hotmail.fi>
2022-09-01 17:05:19 +0300

    7.4 - 7.8

--- osa7/anekdootit/frontend/src/App.js
+++ osa7/anekdootit/frontend/src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useField } from './hooks'
 
 import {
   Routes,
@@ -20,10 +21,6 @@ const Menu = () => {
       <Link style={padding} to="/about">about</Link>
     </div>
   )
-}
-
-const Notification = () => {
-
 }
 
 const Anecdote = ({ anecdote }) => {
@@ -72,22 +69,28 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('url')
 
   const navigate = useNavigate()
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
 
     navigate("/")
+  }
+
+  const reset = () => {
+    content.reset()
+    author.reset()
+    info.reset()
   }
 
   return (
@@ -96,17 +99,18 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input {...content.noReset(content)} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...author.noReset(author)} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e) => setInfo(e.target.value)} />
+          <input {...info.noReset(info)} />
         </div>
         <button>create</button>
+        <button type='button' onClick={reset} >reset</button>
       </form>
     </div>
   )
---
+++ osa7/anekdootit/frontend/src/hooks/index.js
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+
+export const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  const reset = () => {
+    setValue('')
+  }
+
+  const noReset = (props) => {
+    return {
+      type: props.type,
+      value: props.value,
+      onChange: props.onChange
+    }
+  }
+
+  return {
+    type,
+    value,
+    onChange,
+    reset,
+    noReset
+  }
+}
