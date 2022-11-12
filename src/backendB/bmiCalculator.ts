7c43077a4f073ebf598da31136a7223514960bd5
DeeCaaD <perttu.kangas@hotmail.fi>
2022-11-12 17:27:15 +0200

    9.1-9.3

---
+++ src/backendB/bmiCalculator.ts
@@ -0,0 +1,43 @@
+interface BmiValues {
+  height: number
+  weight: number
+}
+
+const parseArgumentsBmi = (args: Array<string>): BmiValues => {
+  if (args.length < 4) throw new Error('Not enough arguments')
+  if (args.length > 4) throw new Error('Too many arguments')
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3]),
+    }
+  } else {
+    throw new Error('Provided values were not numbers!')
+  }
+}
+
+const calculateBmi = (height: number, weight: number): string => {
+  const bmi = (weight / height / height) * 10000
+  if (bmi < 18.5) {
+    return 'Underweight'
+  } else if (bmi < 25) {
+    return 'Normal'
+  } else if (bmi < 30) {
+    return 'Overweight'
+  } else if (bmi >= 30) {
+    return 'Obese'
+  }
+  throw new Error('BMI was invalid')
+}
+
+try {
+  const { height, weight } = parseArgumentsBmi(process.argv)
+  console.log(calculateBmi(height, weight))
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.'
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message
+  }
+  console.log(errorMessage)
+}
---
+++ src/backendB/exerciseCalculator.ts
@@ -0,0 +1,66 @@
+interface Exercises {
+  periodLength: number
+  trainingDays: number
+  success: boolean
+  rating: number
+  ratingDescription: string
+  target: number
+  average: number
+}
+
+const parseArgumentsExercise = (args: Array<string>): Array<number> => {
+  if (args.length < 3) throw new Error('Not enough arguments')
+
+  const exerciseDays: Array<number> = []
+
+  for (let i = 2; i < args.length; i++) {
+    if (!isNaN(Number(args[i]))) {
+      exerciseDays.push(Number(args[i]))
+      continue
+    }
+    throw new Error('Provided values were not numbers!')
+  }
+  return exerciseDays
+}
+
+const calculateAverage = (numberList: Array<number>): number => {
+  return numberList.reduce((a, b) => a + b, 0) / numberList.length
+}
+
+const calculateExercises = (exerciseDays: Array<number>): Exercises => {
+  const avg = calculateAverage(exerciseDays)
+
+  const target = 2
+  let rating
+  let ratingDescription
+  if (avg < target) {
+    rating = 1
+    ratingDescription = 'go gym'
+  } else if (avg > target * 2) {
+    rating = 3
+    ratingDescription = 'goes to gym'
+  } else {
+    rating = 2
+    ratingDescription = 'has been in gym'
+  }
+
+  return {
+    periodLength: exerciseDays.length,
+    trainingDays: exerciseDays.filter((i) => i > 0).length,
+    success: avg > target,
+    rating,
+    ratingDescription,
+    target,
+    average: avg,
+  }
+}
+
+try {
+  console.log(calculateExercises(parseArgumentsExercise(process.argv)))
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.'
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message
+  }
+  console.log(errorMessage)
+}
