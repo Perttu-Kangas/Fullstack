ce4ebc1332e04807d0a068d4ec70207fc6fe364f
DeeCaaD <perttu.kangas@hotmail.fi>
2022-12-09 16:00:14 +0200

    6

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,4 +1,6 @@
-import { View, StyleSheet, Pressable, Alert } from 'react-native'
+import { Link } from 'react-router-native'
+import { View, StyleSheet } from 'react-native'
+
 import Constants from 'expo-constants'
 import Text from './Text'
 import theme from '../theme'
@@ -6,17 +8,30 @@ import theme from '../theme'
 const styles = StyleSheet.create({
   container: {
     paddingTop: Constants.statusBarHeight + 16,
-    padding: 16,
+    padding: 10,
     backgroundColor: theme.colors.textPrimary,
+  },
+
+  linkContainer: {
+    flexDirection: 'row',
+  },
+
+  link: {
+    padding: 10,
   },
 })
 
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <Pressable onPress={() => Alert.alert('You pressed the text!')}>
-        <Text color='textWhite'>Repositories</Text>
-      </Pressable>
+      <View style={styles.linkContainer}>
+        <Link to='/' style={styles.link}>
+          <Text color='textWhite'>Repositories</Text>
+        </Link>
+        <Link to='/signin' style={styles.link}>
+          <Text color='textWhite'>Sign in</Text>
+        </Link>
+      </View>
     </View>
   )
 }
