3eaf3ec6b209762bf90c44b6000265c5d35a60cd
DeeCaaD <perttu.kangas@hotmail.fi>
2022-12-13 19:18:50 +0200

    18

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -44,6 +44,17 @@ const initialValues = {
   password: '',
 }
 
+export const SignInContainer = ({ onSubmit }) => {
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+      validationSchema={validationSchema}>
+      {({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
+    </Formik>
+  )
+}
+
 const SignIn = () => {
   const [signIn] = useSignIn()
   const navigate = useNavigate()
@@ -59,14 +70,7 @@ const SignIn = () => {
     }
   }
 
-  return (
-    <Formik
-      initialValues={initialValues}
-      onSubmit={onSubmit}
-      validationSchema={validationSchema}>
-      {({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
-    </Formik>
-  )
+  return <SignInContainer onSubmit={onSubmit} />
 }
 
 export default SignIn
