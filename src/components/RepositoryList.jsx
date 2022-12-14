deca7a79db7635bcb3f31c30b70f5579ec2149f8
DeeCaaD <perttu.kangas@hotmail.fi>
2022-12-14 20:45:58 +0200

    23

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,6 +1,8 @@
 import { View, FlatList, StyleSheet } from 'react-native'
 import useRepositories from '../hooks/useRepositories'
 import RepositoryItemList from './RepositoryItemList'
+import { useState } from 'react'
+import { RadioButton, List } from 'react-native-paper'
 
 const styles = StyleSheet.create({
   separator: {
@@ -10,7 +12,57 @@ const styles = StyleSheet.create({
 
 const ItemSeparator = () => <View style={styles.separator} />
 
-export const RepositoryListContainer = ({ repositories }) => {
+const RepositorySorter = ({ setOrder }) => {
+  const [expanded, setExpanded] = useState(true)
+  const [value, setValue] = useState('Latest repositories')
+
+  const handlePress = () => setExpanded(!expanded)
+
+  const onSortingPress = (value) => {
+    setValue(value)
+    if (value === 'Highest rated repositories') {
+      setOrder({
+        orderDirection: 'DESC',
+        orderBy: 'RATING_AVERAGE',
+      })
+    } else if (value === 'Lowest rated repositories') {
+      setOrder({
+        orderDirection: 'ASC',
+        orderBy: 'RATING_AVERAGE',
+      })
+    } else {
+      setOrder({
+        orderDirection: 'DESC',
+        orderBy: 'CREATED_AT',
+      })
+    }
+  }
+
+  return (
+    <List.Section>
+      <List.Accordion title={value} onPress={handlePress}>
+        <RadioButton.Group
+          onValueChange={(newValue) => onSortingPress(newValue)}
+          value={value}>
+          <RadioButton.Item
+            label='Latest repositories'
+            value='Latest repositories'
+          />
+          <RadioButton.Item
+            label='Highest rated repositories'
+            value='Highest rated repositories'
+          />
+          <RadioButton.Item
+            label='Lowest rated repositories'
+            value='Lowest rated repositories'
+          />
+        </RadioButton.Group>
+      </List.Accordion>
+    </List.Section>
+  )
+}
+
+export const RepositoryListContainer = ({ repositories, setOrder }) => {
   const renderItem = ({ item }) => <RepositoryItemList repo={item} />
 
   const repositoryNodes = repositories
@@ -23,14 +75,22 @@ export const RepositoryListContainer = ({ repositories }) => {
       ItemSeparatorComponent={ItemSeparator}
       renderItem={renderItem}
       keyExtractor={(item) => item.id}
+      ListHeaderComponent={() => <RepositorySorter setOrder={setOrder} />}
     />
   )
 }
 
 const RepositoryList = () => {
-  const { repositories } = useRepositories()
+  const [order, setOrder] = useState({
+    orderDirection: 'DESC',
+    orderBy: 'RATING_AVERAGE',
+  })
 
-  return <RepositoryListContainer repositories={repositories} />
+  const { repositories } = useRepositories(order)
+
+  return (
+    <RepositoryListContainer repositories={repositories} setOrder={setOrder} />
+  )
 }
 
 export default RepositoryList
--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -1,10 +1,13 @@
 import { useQuery } from "@apollo/client";
 import { GET_REPOSITORIES } from "../graphql/queries";
 
-const useRepositories = () => {
+const useRepositories = (sort) => {
 
     const { data, ...result } = useQuery(GET_REPOSITORIES, {
         fetchPolicy: 'cache-and-network',
+        variables: {
+            ...sort
+        }
     });
 
     return { repositories: data ? data.repositories : undefined, ...result };
